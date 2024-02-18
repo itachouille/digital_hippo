d9a0b14caa2e64b13c8ea0209938a42fb6d37c55
Anthony David <anthony.david34@sfr.fr>
2024-02-18 15:20:33 +0100

    sign-in & sign-up page

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -46,8 +46,8 @@ export const getPayloadClient = async ({
     cached.promise = payload.init({
       email: {
         transport: transporter,
-        fromAddress: 'anthony.david34@sfr.fr',
-        fromName: 'DigitalHippo',
+        fromAddress: 'onboarding@resend.dev',
+        fromName: 'DigitalHippo', 
       },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
